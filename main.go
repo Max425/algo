7eeda562a23fb5c5d9cd12cc89219e7707457ec7
Maxim Sikanov <sikanov.maksim@wb.ru>
2024-05-05 17:03:21 +0300

    wb tests

--- main.go
+++ main.go
@@ -1,23 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
-func powNums(nums []int) []int {
-	res := make([]int, len(nums))
-	left, right := 0, len(nums)-1
-	for left <= right {
-		if nums[left]*nums[left] > nums[right]*nums[right] {
-			res[right-left] = nums[left] * nums[left]
-			left++
-		} else {
-			res[right-left] = nums[right] * nums[right]
-			right--
-		}
-	}
-	return res
+type Record struct {
+	Query               string `json:"Query"`
+	NormQuery           string `json:"NormQuery"`
+	Miner               string `json:"Miner"`
+	MinerArgs           string `json:"MinerArgs"`
+	SortArgs            string `json:"SortArgs"`
+	ShardKey            string `json:"ShardKey"`
+	ShardQuery          string `json:"ShardQuery"`
+	PresetArgs          string `json:"PresetArgs"`
+	Status              string `json:"Status"`
+	ABGroup             string `json:"ABGroup"`
+	ABKey               string `json:"ABKey"`
+	ABLabel             string `json:"ABLabel"`
+	MasterShardKey      string `json:"MasterShardKey"`
+	MasterShardQuery    string `json:"MasterShardQuery"`
+	MetaInfo            string `json:"MetaInfo"`
+	ShardQuery2         string `json:"ShardQuery2"`
+	SearchFilter        string `json:"SearchFilter"`
+	DefaultSearchFilter struct {
+		Filter string `json:"Filter"`
+		Ver    int    `json:"Ver"`
+	} `json:"DefaultSearchFilter"`
+	UseShardQuery int      `json:"UseShardQuery"`
+	Version       int      `json:"Version"`
+	QueryAnalysis string   `json:"QueryAnalysis"`
+	Embedding     []string `json:"Embedding"`
+	PresetConfig  *string  `json:"PresetConfig"`
+	PresetFilter  *string  `json:"PresetFilter"`
+	Promo         bool     `json:"Promo"`
+	Delete        bool     `json:"Delete"`
+	DeleteQueue   bool     `json:"DeleteQueue"`
+	FromQueue     bool     `json:"FromQueue"`
+	NotMining     bool     `json:"NotMining"`
+	PushExact     bool     `json:"PushExact"`
+}
+
+type Data struct {
+	Author string    `json:"author"`
+	Rv     time.Time `json:"rv"`
 }
 
 func main() {
-	nums := []int{-124, -23, -11, 0, 12, 12, 13, 35, 234}
-	fmt.Println(powNums(nums))
+	jsonData := `{
+        "Record": {
+            "Query": "литературный клуб",
+            "NormQuery": "subj_клуб литературный",
+            "Miner": "context;context-v2",
+            "MinerArgs": "--context-subject=\"книги\" --max-product=35000 --query=\"литературный клуб\" --context-subject-filter=true --use-hash-tokens=true;--context-subject=\"книги\" --max-product=35000 --query=\"литературный клуб\" --context-subject-filter=true --use-hash-tokens=true",
+            "SortArgs": "",
+            "ShardKey": "presets/bucket_653",
+            "ShardQuery": "preset=18922401",
+            "PresetArgs": "",
+            "Status": "ok",
+            "ABGroup": "",
+            "ABKey": "",
+            "ABLabel": "",
+            "MasterShardKey": "",
+            "MasterShardQuery": "",
+            "MetaInfo": "",
+            "ShardQuery2": "",
+            "SearchFilter": "",
+            "DefaultSearchFilter": {
+                "Filter": "",
+                "Ver": 0
+            },
+            "UseShardQuery": 0,
+            "Version": 2,
+            "QueryAnalysis": "",
+            "Embedding": [],
+            "PresetConfig": null,
+            "PresetFilter": null,
+            "Promo": false,
+            "Delete": false,
+            "DeleteQueue": false,
+            "FromQueue": false,
+            "NotMining": false,
+            "PushExact": false
+        },
+        "author": "gubeydullin.i",
+        "rv": "2024-02-27T18:02:39+03:00"
+    }`
+
+	var data Data
+	err := json.Unmarshal([]byte(jsonData), &data)
+	if err != nil {
+		fmt.Println("Ошибка при разборе JSON:", err)
+		return
+	}
+
+	// Вывод данных из объекта
+	fmt.Println("Author:", data.Author)
+	fmt.Println("Rv:", data.Rv)
 }
--- sync.go
+++ sync.go
@@ -1,51 +1,51 @@
 package main
 
-// Задача "Асинхронное слияние каналов"
-// Реализовать функцию asyncMerge, которая будет производить асинхронное слияние каналов.
+// // Задача "Асинхронное слияние каналов"
+// // Реализовать функцию asyncMerge, которая будет производить асинхронное слияние каналов.
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-// Исходные данные:
-func main() {
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 20)
+// // Исходные данные:
+// func main() {
+// 	ch1 := make(chan int, 10)
+// 	ch2 := make(chan int, 20)
 
-	ch1 <- 1
-	ch2 <- 2
-	ch2 <- 4
+// 	ch1 <- 1
+// 	ch2 <- 2
+// 	ch2 <- 4
 
-	close(ch1)
-	close(ch2)
+// 	close(ch1)
+// 	close(ch2)
 
-	ch3 := asyncMerge[int](ch1, ch2)
+// 	ch3 := asyncMerge[int](ch1, ch2)
 
-	for val := range ch3 {
-		fmt.Println(val)
-	}
-}
+// 	for val := range ch3 {
+// 		fmt.Println(val)
+// 	}
+// }
 
-func asyncMerge[T any](chans ...chan T) chan T {
-	ch := make(chan T)
-	wg := &sync.WaitGroup{}
+// func asyncMerge[T any](chans ...chan T) chan T {
+// 	ch := make(chan T)
+// 	wg := &sync.WaitGroup{}
 
-	for _, el := range chans {
-		wg.Add(1)
+// 	for _, el := range chans {
+// 		wg.Add(1)
 
-		go func(el chan T) {
-			for x := range el {
-				ch <- x
-			}
-			wg.Done()
-		}(el)
-	}
+// 		go func(el chan T) {
+// 			for x := range el {
+// 				ch <- x
+// 			}
+// 			wg.Done()
+// 		}(el)
+// 	}
 
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
+// 	go func() {
+// 		wg.Wait()
+// 		close(ch)
+// 	}()
 
-	return ch
-}
+// 	return ch
+// }
